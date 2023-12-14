3eab7ac0057a73a045057eb56b208fa1a1f78008
郑楚 <931752763@qq.com>
2023-12-14 16:30:05 +0800

    maven spring init

---
+++ src/main/java/com/zhengchu/llm_chat/LlmChatApplication.java
@@ -0,0 +1,13 @@
+package com.zhengchu.llm_chat;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LlmChatApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LlmChatApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/zhengchu/llm_chat/LlmChatApplicationTests.java
@@ -0,0 +1,13 @@
+package com.zhengchu.llm_chat;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LlmChatApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
